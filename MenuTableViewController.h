c11d3e38d83e6b3d01ddceb697ea90bed704437e
yutaka <yutaka@yutaka-no-MacBook-Air.local>
2014-09-10 21:23:55 +0900

    メニューテーブルを作成。

---
+++ MenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.h
+//  SmartQiita
+//
+//  Created by yutaka on 2014/09/10.
+//  Copyright (c) 2014年 yutaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewController : UITableViewController
+
+@end
