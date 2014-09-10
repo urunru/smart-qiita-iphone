b40ecb20712a1318a6c79aba78f3975cf8627118
yutaka <yutaka@yutaka-no-MacBook-Air.local>
2014-09-10 22:13:47 +0900

    コレクションビューを追加。

---
+++ SmartQiita/TagViewController.h
@@ -0,0 +1,13 @@
+//
+//  TagViewController.h
+//  SmartQiita
+//
+//  Created by yutaka on 2014/09/10.
+//  Copyright (c) 2014年 yutaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagViewController : UICollectionViewController
+
+@end
---
+++ SmartQiita/TagViewController.m
@@ -0,0 +1,49 @@
+//
+//  TagViewController.m
+//  SmartQiita
+//
+//  Created by yutaka on 2014/09/10.
+//  Copyright (c) 2014年 yutaka. All rights reserved.
+//
+
+#import "TagViewController.h"
+
+@interface TagViewController ()
+
+@end
+
+@implementation TagViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
