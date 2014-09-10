1ee2270e1b254f06ebc499c7f9141c7220d85ca2
yutaka <yutaka@yutaka-no-MacBook-Air.local>
2014-09-10 17:46:18 +0900

    Initial Commit

---
+++ SmartQiita/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartQiita
+//
+//  Created by yutaka on 2014/09/10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartQiita/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartQiita
+//
+//  Created by yutaka on 2014/09/10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
